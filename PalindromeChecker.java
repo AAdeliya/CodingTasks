211095f75a91ca719951c492aa6afcc558bc5974
firstPush <adel.sdet2@gmail.com>
2024-06-16 22:16:09 -0500

    in process

---
+++ PalindromeChecker.java
@@ -0,0 +1,14 @@
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        String input = "adadadadadaaaaaadddadad";
+        boolean Palindranomic;
+        char ch;
+
+        for (int i = 0; i < input.length(); i++) {
+            
+
+            
+        }
+    }
+    
+}
