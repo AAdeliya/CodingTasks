08d72005a6f1848df826c89e5e61e058450bfaaa
firstPush <adel.sdet2@gmail.com>
2024-06-20 13:23:29 -0500

    palindranomic

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,14 +1,21 @@
-// public class PalindromeChecker {
-//     public static void main(String[] args) {
-//         String input = "adadadadadaaaaaadddadad";
-//         boolean Palindranomic;
-//         char ch;
+public class PalindromeChecker {
+    public static void main(String[] args) {
+       
+        System.out.println(IsPalindranomic("kssk"));
 
-//         for (int i = 0; i < input.length(); i++) {
-            
+    }
+     public static boolean IsPalindranomic(String input){ 
+       
+        int leftPointer=0; //start
+        int rightPointer=input.length()-1; //end
+        while(leftPointer<rightPointer){
+            if(input.charAt(leftPointer)!= input.charAt(rightPointer)){
+                return false;
+            }
+            leftPointer++;
+            rightPointer--;
 
-            
-//         }
-//     }
+        }
+        return true;
     
-// }
+}}
