8dd60b87b710ee7853d1e704b1f98c32e85ce326
firstPush <adel.sdet2@gmail.com>
2024-07-05 15:17:55 -0500

    cc

---
+++ ATMInterface.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+//balance, deposit, and withdraw money
+
+public class ATMInterface {
+    int balance = 1000;
+    String  deposit;
+    String  withdraw;
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("What kind of operation would you like to do?");
+
+        //int balance = userInput.nextInt();
+        //if(userInput== balance){
+            
+
+    }
+
+        public int balanceCheck(){
+            System.out.println("Your balance is :" + balance);
+            return balance;
+
+        }
+        public int deposit(int d){
+            balance+=d;
+            System.out.println("Your balance is updated : " + balance);
+            return balance;
+            
+        }
+        public int withdraw(int w){
+            balance-=w;
+            System.out.println("Your balance is updated : " + balance);
+            return balance;
+
+        }
+
+
+       
+        
+        
+       
+    
+}
