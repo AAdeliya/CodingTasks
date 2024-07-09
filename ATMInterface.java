2815748264eef48b72ecaddd31eff03339c8a0a7
firstPush <adel.sdet2@gmail.com>
2024-07-09 18:56:24 -0500

    cc

--- ATMInterface.java
+++ ATMInterface.java
@@ -3,37 +3,51 @@ import java.util.Scanner;
 //balance, deposit, and withdraw money
 
 public class ATMInterface {
-    int balance = 1000;
-    String  deposit;
-    String  withdraw;
+    private static double balance = 0;
+    private double withdraw = 0;
+    private double deposit = 0;
+    private double amount = 0;
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         
         System.out.println("What kind of operation would you like to do?");
 
-        //int balance = userInput.nextInt();
+        int enterchoice = input.nextInt();    
         //if(userInput== balance){
-            //
+            
 
+    if (enterchoice == 1) {
+        System.out.println("The balance is " + balanceCheck());       
+      } else if (enterchoice == 2) {
+        System.out.println("Enter an amount to withdraw ");
+        double amount = input.nextDouble();
+        //withdraw method
+        withdraw(45);
+
+      } else if (enterchoice == 3) {
+        System.out.println("Enter an amount to deposit ");
+        double amount = input.nextDouble();
+        //deposit method
+        deposit(50);
+      } 
     }
+    
 
-        public int balanceCheck(){
-            System.out.println("Your balance is :" + balance);
+        public static double balanceCheck(){
             return balance;
 
         }
-        public int deposit(int d){
-            balance+=d;
-            System.out.println("Your balance is updated : " + balance);
-            return balance;
+        public static double deposit(double amount){
+            return balance+=amount;
+            
+            
             
         }
-        public int withdraw(int w){
-            balance-=w;
-            System.out.println("Your balance is updated : " + balance);
-            return balance;
+        public static double withdraw(double amount){
+            return balance-=amount;
+            
 
         }
 
