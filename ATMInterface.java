63e159802bb4f5c8dd233aa7327a3a71419fe6bc
firstPush <adel.sdet2@gmail.com>
2024-07-09 19:18:25 -0500

    cc

--- ATMInterface.java
+++ ATMInterface.java
@@ -6,35 +6,37 @@ public class ATMInterface {
     private static double balance = 0;
     private double withdraw = 0;
     private double deposit = 0;
-    private double amount = 0;
+    public static double amount = 0;
 
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
-        System.out.println("What kind of operation would you like to do?");
+        int enterchoice=9;  //any number 
 
-        int enterchoice = input.nextInt();    
-        //if(userInput== balance){
+        while(enterchoice != 0)  {
+        System.out.println("What kind of operation would you like to do?");
+        enterchoice = input.nextInt();   
+        
+        if (enterchoice == 1) {
+            System.out.println("The balance is " + balanceCheck());       
+          } else if (enterchoice == 2) {
+            System.out.println("Enter an amount to withdraw ");
+            amount = input.nextDouble();
+            //withdraw method
+            System.out.println("The balance is " + withdraw(amount));       
             
 
-    if (enterchoice == 1) {
-        System.out.println("The balance is " + balanceCheck());       
-      } else if (enterchoice == 2) {
-        System.out.println("Enter an amount to withdraw ");
-        double amount = input.nextDouble();
-        //withdraw method
-        withdraw(45);
-
-      } else if (enterchoice == 3) {
-        System.out.println("Enter an amount to deposit ");
-        double amount = input.nextDouble();
-        //deposit method
-        deposit(50);
-      } 
-    }
     
-
+          } else if (enterchoice == 3) {
+            System.out.println("Enter an amount to deposit ");
+            amount = input.nextDouble();
+            //deposit method
+            System.out.println("The balance is " + deposit(amount));       
+           
+          } 
+        }
+    }
+            
         public static double balanceCheck(){
             return balance;
 
